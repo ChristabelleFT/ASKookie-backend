5055f622d67629f3292b2e609a159f735b6b1f29
ChristabelleFT <christabelle.ft@gmail.com>
2020-06-24 22:01:24 +0800

    title,post

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const userRouter = require("./command/users/router");
 app.use(express.json());
 
 app.use("", userRouter);
-app.listen(process.env.PORT, '0.0.0.0');
-/*app.listen(process.env.PORT || 5000, () => {
+//app.listen(process.env.PORT, '0.0.0.0');
+app.listen(process.env.PORT || 5000, () => {
     if(process.env.PORT){
         console.log("Server is running on port: ", process.env.PORT);
     } else {
         console.log("Server is running on port: 5000");
     }
-});*/
+});
--- posts.sql
+++ posts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2020 at 07:56 PM
+-- Generation Time: Jun 24, 2020 at 03:58 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -34,29 +34,35 @@ CREATE TABLE `feeds` (
   `post` text NOT NULL,
   `answerer` varchar(15) DEFAULT NULL,
   `answer` text DEFAULT NULL,
-  `type` varchar(10) NOT NULL
+  `type` varchar(10) NOT NULL,
+  `title` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `feeds`
 --
 
-INSERT INTO `feeds` (`postID`, `category`, `asker`, `post`, `answerer`, `answer`, `type`) VALUES
-(1, 'accomodation', 'user1', 'What is the difference between each Residential Colleges?', 'user0', 'The art and cultures are different', ''),
-(2, 'modules', 'user1', 'How is life in NUS?', 'user2', 'Life is good', ''),
-(3, 'faculties', 'user0', 'Why choose SOC?', 'user1', 'Because it is good', ''),
-(4, 'faculties', 'user0', 'Why choose SDE?', 'user0', 'Because it is nice', ''),
-(5, 'Exchange/NOC', 'user0', 'What should I prepare for Exchange?', NULL, NULL, ''),
-(6, 'modules', 'user2', 'What module should I take?', NULL, NULL, ''),
-(7, 'Exchange/NOC', 'user2', 'Should I go for NOC?', 'test3', 'Absolutely!', ''),
-(8, 'job_intern', 'user0', 'What are the opportunities out there?', NULL, NULL, ''),
-(10, 'accommodation', 'user0', 'How is living in NUS?', 'test3', 'It\'s the best', ''),
-(11, 'accommodation', 'user7', 'Is it expensive to live on-campus?', 'user7', 'Yes', ''),
-(12, 'job_intern', 'user7', 'Is it hard to get internships?', NULL, NULL, ''),
-(15, 'faculties', 'test4', 'How many faculties are there is NUS?', NULL, NULL, ''),
-(19, 'student_life', 'test4', 'Is it hard to get into CCA in NUS?', NULL, NULL, ''),
-(20, 'exchange_noc', 'test4', 'Is NOC beter than exchange?', NULL, NULL, ''),
-(23, 'faculties', 'test4', 'What is the most popular major in NUS', NULL, NULL, '');
+INSERT INTO `feeds` (`postID`, `category`, `asker`, `post`, `answerer`, `answer`, `type`, `title`) VALUES
+(1, 'accomodation', 'user1', 'What is the difference between each Residential Colleges?', 'user0', 'The art and cultures are different', '', NULL),
+(2, 'modules', 'user1', 'How is life in NUS?', 'user2', 'Life is good', '', NULL),
+(3, 'faculties', 'user0', 'Why choose SOC?', 'user1', 'Because it is good', '', NULL),
+(4, 'faculties', 'user0', 'Why choose SDE?', 'user0', 'Because it is nice', '', NULL),
+(5, 'Exchange/NOC', 'user0', 'What should I prepare for Exchange?', NULL, NULL, '', NULL),
+(6, 'modules', 'user2', 'What module should I take?', NULL, NULL, '', NULL),
+(7, 'Exchange/NOC', 'user2', 'Should I go for NOC?', 'test3', 'Absolutely!', '', NULL),
+(8, 'job_intern', 'user0', 'What are the opportunities out there?', NULL, NULL, '', NULL),
+(10, 'accommodation', 'user0', 'How is living in NUS?', 'test3', 'It\'s the best', '', NULL),
+(11, 'accommodation', 'user7', 'Is it expensive to live on-campus?', 'user7', 'Yes', '', NULL),
+(12, 'job_intern', 'user7', 'Is it hard to get internships?', NULL, NULL, '', NULL),
+(15, 'faculties', 'test4', 'How many faculties are there is NUS?', NULL, NULL, '', NULL),
+(19, 'student_life', 'test4', 'Is it hard to get into CCA in NUS?', NULL, NULL, '', NULL),
+(20, 'exchange_noc', 'test4', 'Is NOC beter than exchange?', NULL, NULL, '', NULL),
+(23, 'faculties', 'test4', 'What is the most popular major in NUS', NULL, NULL, '', NULL),
+(29, 'accommodation', 'test4', 'Why choose hall over RC?', NULL, NULL, '', NULL),
+(30, 'job_intern', 'test4', 'Do we find our own internships?', NULL, NULL, '', NULL),
+(31, 'others', 'test4', 'What is the best foodcourt in NUS?', NULL, NULL, '', NULL),
+(32, 'faculties', 'test4', 'Why choose Computer science?', NULL, NULL, '', NULL),
+(34, 'student_life', 'test3', 'Do NUS CCAs have a lot of commitments?', NULL, NULL, '', '');
 
 -- --------------------------------------------------------
 
@@ -67,26 +73,27 @@ INSERT INTO `feeds` (`postID`, `category`, `asker`, `post`, `answerer`, `answer`
 CREATE TABLE `users` (
   `username` varchar(12) NOT NULL,
   `password` text DEFAULT NULL,
-  `email` varchar(25) NOT NULL
+  `email` varchar(25) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `password`, `email`) VALUES
-('', '$2b$10$qC.hZ', ''),
-('test', '$2b$10$OgNh9', 'test@test.com'),
-('test2', '$2b$10$Ml/kO', 'test2@test2.com'),
-('test3', '$2b$10$fAla6AseoTy/lwa4shyDT.UfydcDDoaCeshS6jpad.s.d57lwgV8i', 'test3@gmail.com'),
-('test4', '$2b$10$7MjFLDiCUuuHB/oTeL9fB.09XQ.ybypdVZBTCIbF4I1YD6SxNnKWC', 'test4@test.com'),
-('user0', '00', ''),
-('user1', '01', ''),
-('user2', '02', ''),
-('user3', '03', ''),
-('user4', '04', ''),
-('user6', '$2b$10$bbfD1', 'user6@user6.com'),
-('user7', '$2b$10$HTDXdfMd852dT3NmS1xLo.o8b9abkmWwpwitMElI6cxUsdzpVrnqe', 'user7@user.com');
+INSERT INTO `users` (`username`, `password`, `email`, `verified`) VALUES
+('', '$2b$10$qC.hZ', '', 0),
+('test', '$2b$10$OgNh9', 'test@test.com', 0),
+('test2', '$2b$10$Ml/kO', 'test2@test2.com', 0),
+('test3', '$2b$10$fAla6AseoTy/lwa4shyDT.UfydcDDoaCeshS6jpad.s.d57lwgV8i', 'test3@gmail.com', 0),
+('test4', '$2b$10$7MjFLDiCUuuHB/oTeL9fB.09XQ.ybypdVZBTCIbF4I1YD6SxNnKWC', 'test4@test.com', 0),
+('user0', '00', '', 0),
+('user1', '01', '', 0),
+('user2', '02', '', 0),
+('user3', '03', '', 0),
+('user4', '04', '', 0),
+('user6', '$2b$10$bbfD1', 'user6@user6.com', 0),
+('user7', '$2b$10$HTDXdfMd852dT3NmS1xLo.o8b9abkmWwpwitMElI6cxUsdzpVrnqe', 'user7@user.com', 0);
 
 --
 -- Indexes for dumped tables
@@ -114,7 +121,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `feeds`
 --
 ALTER TABLE `feeds`
-  MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- Constraints for dumped tables
