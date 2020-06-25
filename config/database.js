c6688f7aeb705208f77bc0e49d9f8d8dea77f25b
Michela Vieri <michela.vieri.hp@gmail.com>
2020-06-25 23:09:28 +0800

    change port users dkk back to env

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const { createPool } = require("mysql");
 
 const pool = createPool ({
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'b42af6808ff8de',
-    password: '77f4f5a1',
-    database: 'heroku_9202346d5b0ea9b',
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.MYSQL_DB,
     connectionLimit: 50
 });
 
