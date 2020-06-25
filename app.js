7877b2a64dedebfb8ec31f96e751c847d46f3c9e
ChristabelleFT <christabelle.ft@gmail.com>
2020-06-25 15:01:37 +0800

    deploy trial

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./command/users/router");
+const path = require("path");
 
 app.use(express.json());
 
-app.use("", userRouter);
-//app.listen(process.env.PORT, '0.0.0.0');
-//app.listen(process.env.PORT || 5000, () => {
-app.listen(process.env.PORT || '5000', () => {
-    console.log("host", process.env.DB_HOST);
-    if(process.env.PORT){
-        console.log("Server is running on port: ", process.env.PORT);
-    } else {
-        console.log("Server is running on port: 5000");
-    }
-});
+app.use('', userRouter);
+
+//Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('ASKookie_frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'ASKookie_frontend', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
