b127b2f62a3cbd849a04caaca2fdfaf9bc6466a7
ChristabelleFT <christabelle.ft@gmail.com>
2020-07-24 18:41:34 +0800

    upload image

--- command/users/uploadImage.js
+++ command/users/uploadImage.js
@@ -2,42 +2,27 @@ const { cloudinary } = require('../../config/cloudinary_config');
 const { uploadImages } = require("./service");
 
 module.exports = {
-    uploadImg: (req, res) => {
-      //  const body = req.body;
-        console.log(body);
-        const username = req.params.username;
-        const fileStr = req.body.fileString;
+    uploadImg: async (req, res) => {
+        try {
+            const fileStr = req.body.data;
+            const username = req.params.username;
 
-        const uploadResponse = cloudinary.uploader.upload(
-            fileStr, {
-                upload_preset: 'askookie'
-            })
-        
-        console.log("uploadResponse " + uploadResponse);
+            const uploadResponse = await cloudinary.uploader.upload(
+                fileStr, {
+                    upload_preset: 'askookie'
+                })
 
-        const url = uploadResponse.url;
+            const url = uploadResponse.url;
 
-        console.log("uploadResponse2 " + url);
-
-        uploadImages(username, url, (err, results) => {
-            if(err) {
-                console.log(err);
-                return err;
-            }
-            return results;
-        });
-
-        // uploadImages(username, url, (err, results) => {
-        //     if(err) {
-        //         console.log(err);
-        //         return res.status(500).json({
-        //             message: "Database connection error"
-        //         });
-        //     }
-        //     return res.status(200).json({
-        //         data: results,
-        //         message: "Image uploaded"
-        //     });
-        // });
+            await uploadImages(username, url, (err, results) => {
+                if(err) {
+                    console.log(err);
+                    return err;
+                }
+                return results;
+            });
+        } catch (error) {
+            console.error(error); 
+        }
     },
 };
