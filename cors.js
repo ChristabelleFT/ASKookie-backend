2e92c7dce803c72779de6260dfdb27f0a5f18f76
ChristabelleFT <christabelle.ft@gmail.com>
2020-07-27 01:31:19 +0800

    register bug

--- cors.js
+++ cors.js
@@ -1,7 +1,9 @@
 const cors = require('cors');
 
 const allowedOrigin = [
+    '',
     'http://localhost:3000',
+    'http://localhost:3000/profile',
     'http://localhost:5000',
     'http://localhost:3000/',
     'http://localhost:5000/',
@@ -11,14 +13,17 @@ const allowedOrigin = [
     'https://compassionate-shockley-c03462.netlify.app/',
     'https://compassionate-shockley-c03462.netlify.app',
     'https://askookie.netlify.app/',
-    'https://askookie.netlify.app'
+    'https://askookie.netlify.app',
+    'http://cloudinary.com/',
+    'http://cloudinary.com'
 ];
 
 exports.corsConfig = {
     origin: (origin, cb) => {
+        console.log(origin);
         if(!origin) return cb(null, true);
         if(allowedOrigin.indexOf(origin) === -1) {
-            return cb(new Error(`THe origin ${origin} is not allowed to call the askookie API`), false);
+            return cb(new Error(`The origin ${origin} is not allowed to call the askookie API`), false);
         }
         return cb(null, true);
     }
